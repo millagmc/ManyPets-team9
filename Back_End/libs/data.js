b7235fbbd86cfd584ea3a411dba2666275f87b59
Camilla Santos <milla.revolts@gmail.com>
2022-09-02 13:59:00 +0100

    libs

--- Back_End/libs/data.js
+++ Back_End/libs/data.js
@@ -6,9 +6,17 @@ export const dummydata = [
       address: "123 london,uk",
       postcode: "se10 9fy",
       email_address: "santos.Carlos@gmail.com",
-      pet_name: ["Luke", "Juno", "Lupin"],
-      pet_age: [1, 3, 0],
-      breed: ["shihtzu", "Huskey", "Golden Retriver"],
+      pets: [{
+        pet_name: "Luke", 
+        pet_age: 1,
+        breed: "shihtzu"
+      },{
+        pet_name: "Lupin", 
+        pet_age: 0,
+        breed: "chow chow"
+      }],
+      date: new Date()
+   
     },
     {
       id: 2,
