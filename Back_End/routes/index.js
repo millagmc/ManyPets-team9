765239f2c6eec43b52b67711dec809ed412cb1b6
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-09-02 13:52:55 +0100

    post added

--- Back_End/routes/index.js
+++ Back_End/routes/index.js
@@ -32,4 +32,43 @@ router.get('/api/quotes/:id', async function (req, res, next) {
   });
 });
 
+router.get('/api/quotes/client', async function (req, res, next) {
+  const title = req.query;
+  console.log(title);
+
+  // const data = await callId(setId);
+
+  // async function callId(setId) {
+  //   return dummydata.filter((input) => {
+  //     {
+  //       if (input.id === setId) {
+  //         console.log(input);
+  //         return input;
+  //       }
+  //     }
+  //   });
+  // }
+  res.json({
+    message: 'success',
+    payload: { title },
+  });
+});
+
+router.post('/api/quotes', async function (req, res, next) {
+  const title = req.body;
+  console.log(title);
+
+  const data = await postData(title);
+
+  async function postData(title) {
+    dummydata.push(title);
+    return title;
+  }
+
+  res.json({
+    message: 'success',
+    payload: { data },
+  });
+});
+
 export default router;
