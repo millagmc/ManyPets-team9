4312c2179f4c859cd95689159ea01ab0e3244855
tasniim <tasniim@MacBook-Pro.local>
2022-09-02 10:54:19 +0100

    update filename

--- Back_End/app.js
+++ Back_End/app.js
@@ -1,13 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import express from "express";
+import path from "path";
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var app = express();
+import indexRouter from "./routes/index.js"
+import userRouter from "./routes/users.js";
+import cors from "cors";
 
+const app = express();
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
