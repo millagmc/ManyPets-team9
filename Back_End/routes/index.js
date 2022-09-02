fb69c6138ff32840c0e0ba6d5c781859bd1f28b8
Camilla Santos <milla.revolts@gmail.com>
2022-09-02 11:47:26 +0100

    added the route to get the dummy data with the id

--- Back_End/routes/index.js
+++ Back_End/routes/index.js
@@ -4,8 +4,19 @@ const router = express.Router();
 import {dummydata} from "../libs/data.js"
 
 
-router.get('/', function(req, res, next) {
+router.get('/api/quotes', function(req, res, next) {
   // res.render('index', { title: 'Express' });
+  res.json({
+    message: "success",
+    payload: dummydata
+  })
+});
+
+router.get('/api/quotes/:id', function(req, res, next) {
+ 
+  const {id} = req.params
+  const setId = Number(id)
+  console.log(setId)
   res.json({
     message: "success",
     payload: dummydata
